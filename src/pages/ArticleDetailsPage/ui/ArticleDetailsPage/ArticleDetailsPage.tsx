686d2b7313d406f93fae1ba94be5adc1b474f0b2
Vladimir <SonicWz@yandex.ru>
2024-04-05 16:47:49 +0500

    remove husky

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,54 +1,52 @@
-import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
-import { useParams } from 'react-router-dom';
-import { ArticleDetails } from '@/entities/Article';
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { useTranslation } from "react-i18next";
+import { memo } from "react";
+import { useParams } from "react-router-dom";
+import { ArticleDetails } from "@/entities/Article";
+import { classNames } from "@/shared/lib/classNames/classNames";
 import {
-    DynamicModuleLoader,
-    ReducersList,
-} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { Page } from '@/widgets/Page/Page';
-import { articleDetailsPageReducer } from '../../model/slices';
-import cls from './ArticleDetailsPage.module.scss';
-import { ArticleDetailsPageHeader } from './ArticleDetailsPageHeader/ArticleDetailsPageHeader';
-import { VStack } from '@/shared/ui/Stack';
-import { ArticleRecommendationList } from '@/features/ArticleRecommendationList';
-import { ArticleDetailsComments } from './ArticleDetailsComments/ArticleDetailsComments';
-import { ArticleRating } from '@/features/ArticleRating';
-import { getFeaturesFlags } from '@/shared/lib/features';
+  DynamicModuleLoader,
+  ReducersList,
+} from "@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
+import { Page } from "@/widgets/Page/Page";
+import { articleDetailsPageReducer } from "../../model/slices";
+import cls from "./ArticleDetailsPage.module.scss";
+import { ArticleDetailsPageHeader } from "./ArticleDetailsPageHeader/ArticleDetailsPageHeader";
+import { VStack } from "@/shared/ui/Stack";
+import { ArticleRecommendationList } from "@/features/ArticleRecommendationList";
+import { ArticleDetailsComments } from "./ArticleDetailsComments/ArticleDetailsComments";
+import { ArticleRating } from "@/features/ArticleRating";
+import { getFeaturesFlags } from "@/shared/lib/features";
 interface ArticleDetailsPageProps {
-    className?: string;
+  className?: string;
 }
 
 const reducers: ReducersList = {
-    articleDetailsPage: articleDetailsPageReducer,
+  articleDetailsPage: articleDetailsPageReducer,
 };
 
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
-    const { className } = props;
-    const { t } = useTranslation('article-details');
-    const { id } = useParams<{ id: string }>();
-    const isArticleRatingEnabled = getFeaturesFlags('isArticleRatingEnabled');
+  const { className } = props;
+  const { t } = useTranslation("article-details");
+  const { id } = useParams<{ id: string }>();
+  const isArticleRatingEnabled = getFeaturesFlags("isArticleRatingEnabled");
 
-    if (!id) {
-        return null;
-    }
+  if (!id) {
+    return null;
+  }
 
-    return (
-        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <Page
-                className={classNames(cls.ArticleDetailsPage, {}, [className])}
-            >
-                <VStack gap="16" max>
-                    <ArticleDetailsPageHeader />
-                    <ArticleDetails id={id} />
-                    {isArticleRatingEnabled && <ArticleRating articleId={id} />}
-                    <ArticleRecommendationList />
-                    <ArticleDetailsComments id={id} />
-                </VStack>
-            </Page>
-        </DynamicModuleLoader>
-    );
+  return (
+    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+      <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+        <VStack gap="16" max>
+          <ArticleDetailsPageHeader />
+          <ArticleDetails id={id} />
+          {isArticleRatingEnabled && <ArticleRating articleId={id} />}
+          <ArticleRecommendationList />
+          <ArticleDetailsComments id={id} />
+        </VStack>
+      </Page>
+    </DynamicModuleLoader>
+  );
 };
 
 export default memo(ArticleDetailsPage);
